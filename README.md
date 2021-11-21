c73ea74525519c4a73cb5c9efab35818de3478e8
min <0s.dongmin@gmail.com>
2021-11-22 02:51:14 +0900

    Implement replace script

--- README.md
+++ README.md
@@ -6,15 +6,21 @@ $ yarn install
 ## Usage
 #### Compile TS file to JS
 ```bash
-$ yarn compile {input file path}
+$ yarn compile {input .ts file path}
 ```
 Example: `$ yarn compile input/quicksort.ts`
 
-#### Generate type dictionary from JS file
+#### Generate type storage from JS/TS file
 ```bash
-$ yarn dict {input file path} {output file path}
+$ yarn dict {input .js/.ts file path} {output .json storage path}
 ```
 Example
 - `$ yarn dict input/quicksort.js output/data.json`
-- `$ yarn dict input/quicksort.ts output/data.json`
+
+#### Replace types
+```bash
+$ yarn replace {input .json storage path} {input .js/.ts file path} {output .ts file path}
+```
+Example
+- `$ yarn replace output/data.json input/quicksort.js output/quicksort.ts`
 
--- script/replace.ts
+++ script/replace.ts
@@ -1,29 +1,27 @@
 import * as ts from 'typescript'
 import * as fs from 'fs'
-import { TDictionary } from '../type'
 import { inCoverage, decodeType } from '../util'
 
-function visit(node: ts.Node, dict: TDictionary) {
+function visit(node: ts.Node, storage: [string, string][]) {
   const result = inCoverage(node)
   if (result) {
-    const [target, convert] = result
-    convert(decodeType(dict[target.pos]))
-  } else {
-    ts.forEachChild(node, (e) => visit(e, dict))
+    const [, , convert] = result
+    convert(decodeType(storage.shift()![1]))
   }
+  ts.forEachChild(node, (e) => visit(e, storage))
 }
 
 function compile(fileName: string, options: ts.CompilerOptions): void {
   const dict = JSON.parse(fs.readFileSync(process.argv[2], 'utf-8'))
   let program = ts.createProgram([fileName], options)
   const sourceFile = program.getSourceFile(fileName)!
-  const resultFile = ts.createSourceFile(process.argv[4], '', ts.ScriptTarget.Latest, false, ts.ScriptKind.TS);
+  const resultFile = ts.createSourceFile(process.argv[4], '', ts.ScriptTarget.Latest, false, ts.ScriptKind.TS)
   // @ts-ignore
   resultFile.statements = sourceFile.statements
   visit(resultFile, dict)
-  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+  const printer = ts.createPrinter({newLine: ts.NewLineKind.LineFeed})
   const text = printer.printNode(ts.EmitHint.Unspecified, resultFile, resultFile)
-  fs.writeFileSync(process.argv[4], text, "utf8");
+  fs.writeFileSync(process.argv[4], text, 'utf8')
 }
 
 compile(process.argv[3], {
