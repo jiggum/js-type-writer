0bd61833fc86fa4c3260e4e703e4dafd1a3b270e
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2021-12-03 13:33:26 +0900

    wip: 시딩 단계에서 추론된 타입을 미리 채워넣기

--- src/seed.ts
+++ src/seed.ts
@@ -1,4 +1,7 @@
 import * as ts from 'typescript'
+import * as path from 'path'
+import * as fs from 'fs'
+
 import { inCoverage, randomType, traverse } from 'src/util'
 
 const DUMMY_FILE_PATH = '/tmp.ts'
@@ -16,3 +19,28 @@ export const randomSeed = (text: string) => {
 
   return root
 }
+
+const oldInputPath = path.join(__dirname, '../input/quicksort.js')
+const inputPath = path.join(__dirname, '../input/quicksort.ts')
+
+fs.copyFileSync(oldInputPath, inputPath)
+
+const program = ts.createProgram({
+  options: {
+    allowJs: true,
+    checkJs: true,
+  },
+  rootNames: [inputPath],
+})
+
+const checker = program.getTypeChecker()
+
+function handler(node: ts.Node) {
+  if (ts.isVariableDeclaration(node)) {
+    console.log(
+      checker.typeToString(checker.getApparentType(checker.getTypeAtLocation(node))).toLowerCase(),
+    )
+  }
+}
+
+traverse(program.getSourceFile(inputPath)!, handler)
