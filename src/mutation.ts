f5cc08e11d6dcd374a1d4c57bf04b1c64fdf6312
김재성 (Kim, Jaesung) <15705385+sundoufu@users.noreply.github.com>
2021-12-16 22:14:47 +0900

    타입 에러가 있는 노드에 대해서만 mutate 하도록 수정

--- src/mutation.ts
+++ src/mutation.ts
@@ -1,29 +1,65 @@
 import * as ts from 'typescript'
-import { inCoverage, isSameType, randomType, traverse } from 'src/util'
+import { appendFileSync } from 'fs'
+import { decodeNodeKind, inCoverage, isFunction, isSameType, randomType, traverse } from 'src/util'
 
-export const mutateTransform = (root: ts.Node, p: number) => {
-  traverse(root, (node) => {
+function formatMutateLog(name: string, prevType: ts.TypeNode | undefined, newType: ts.TypeNode) {
+  return `${name} ${decodeNodeKind(prevType?.kind ?? 0)} ${decodeNodeKind(newType.kind)}\n`
+}
+
+export const inMutationCoverage = (
+  node: ts.Node,
+  diagnostics: ts.Diagnostic[] | undefined,
+  logging = false,
+):
+  | undefined
+  | [
+      string,
+      (to: ts.Node) => void,
+      ts.TypeNode | undefined,
+      (node?: ts.TypeNode) => ts.TypeNode | undefined,
+    ] => {
+  const matchedDiagnostic = diagnostics?.find((d) => d.start === node.pos)
+  if (!diagnostics || matchedDiagnostic) {
     const result = inCoverage(node)
+    if (logging && result && matchedDiagnostic) {
+      const [name] = result
+      console.log(
+        `${name} of type ${decodeNodeKind(result[2]?.kind ?? 0)} has ERROR: ${
+          matchedDiagnostic?.messageText
+        }`,
+      )
+    }
+    return result
+  }
+  return undefined
+}
+export const mutateTransform = (root: ts.Node, p: number, diagnostics?: ts.Diagnostic[]) => {
+  traverse(root, (node) => {
+    const result = inMutationCoverage(node, diagnostics)
     if (result && Math.random() < p) {
-      const [, convert] = result
-      convert(randomType())
+      const [name, convert, prevType] = result
+      const newType = randomType(false, true, isFunction(node))
+      appendFileSync('mutate.transform.log', formatMutateLog(name, prevType, newType))
+      convert(newType)
     }
   })
 }
 
-export const mutateUnion = (root: ts.Node, p: number) => {
+export const mutateUnion = (root: ts.Node, p: number, diagnostics?: ts.Diagnostic[]) => {
   traverse(root, (node) => {
-    const result = inCoverage(node)
+    const result = inMutationCoverage(node, diagnostics)
     if (result && Math.random() < p) {
-      const [, convert, prevType] = result
+      const [name, convert, prevType] = result
       const prevTypes = (() => {
         if (ts.isUnionTypeNode(prevType!)) return prevType.types
         return [prevType!]
       })()
       for (let i = 0; i < 10; i = i + 1) {
-        const type = randomType(false, false)
+        const type = randomType(false, false, isFunction(node))
         if (!prevTypes.some((e) => isSameType(e, type))) {
-          convert(ts.factory.createUnionTypeNode([...prevTypes, type]))
+          const newType = ts.factory.createUnionTypeNode([...prevTypes, type])
+          appendFileSync('mutate.transform.log', formatMutateLog(name, prevType, newType))
+          convert(newType)
           break
         }
       }
@@ -31,18 +67,24 @@ export const mutateUnion = (root: ts.Node, p: number) => {
   })
 }
 
-export const mutateSharpen = (root: ts.Node, p: number) => {
+export const mutateSharpen = (root: ts.Node, p: number, diagnostics?: ts.Diagnostic[]) => {
   traverse(root, (node) => {
-    const result = inCoverage(node)
+    const result = inMutationCoverage(node, diagnostics)
     if (result && Math.random() < p) {
-      const [, convert, prevType] = result
+      const [name, convert, prevType] = result
       if (ts.isUnionTypeNode(prevType!)) {
         const nextTypes = [...prevType.types]
         nextTypes.splice(Math.floor(Math.random() * nextTypes.length), 1)
-        convert(ts.factory.createUnionTypeNode(nextTypes))
+        const newType = ts.factory.createUnionTypeNode(nextTypes)
+        for (const t of prevType.types) {
+          appendFileSync('mutate.transform.log', formatMutateLog(name, t, newType))
+        }
+        convert(newType)
       }
       if (ts.isArrayTypeNode(prevType!)) {
-        convert(ts.factory.createArrayTypeNode(randomType(true, false)))
+        const newType = ts.factory.createArrayTypeNode(randomType(true, false, isFunction(node)))
+        appendFileSync('mutate.transform.log', formatMutateLog(name, prevType, newType))
+        convert(newType)
       }
     }
   })
